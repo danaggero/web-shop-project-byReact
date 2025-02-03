bff715e89a5a81a329fc8150c49e0dc60dd8905b
이지원 <daggero@naver.com>
2025-02-03 22:39:29 +0900

    Merge Conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import "./App.css"
 import {Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-<<<<<<< HEAD
 import Register from "./pages/Register";
-=======
 import NotFound from "./pages/NotFound";
->>>>>>> 5d9e0d85d7af0b5357a4acf7190eef6436c91f2a
 
 
 
@@ -16,11 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
         <Route path="/register" element={<Register />} />
-=======
         <Route path="/NotFound" element={<NotFound/>}/>
->>>>>>> 5d9e0d85d7af0b5357a4acf7190eef6436c91f2a
       </Routes>
     </>
   )
