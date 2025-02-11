5f730c32b41329b20b2880104c485ca8a9db8015
이지원 <daggero@naver.com>
2025-02-12 02:09:47 +0900

    feat: login 구현 및 authStore 생성성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import LoginForm2 from "../components/LoginForm2";
 
 
 const Login = () => {
+
+
   return (
     <div id="wrap" className="mx-auto w-[1280px] h-[1400px] bg-gray-100">
       <Header />
