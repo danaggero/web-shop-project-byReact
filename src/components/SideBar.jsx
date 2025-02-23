e6b284c8dcc4ec4152625e6da181fa0aa8f26f88
이지원 <daggero@naver.com>
2025-02-23 21:27:58 +0900

    fix: sidebar, 동적 UI 구현

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -17,38 +17,42 @@ function SideBar({ isSidebarOpen, setIsSidebarOpen }) {
         className={`fixed top-0 left-0 h-full w-96 bg-white shadow-lg transition-transform duration-300 z-50 
         ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
-        <div className="p-5 flex justify-between items-center border-b h-24">
+        <div className="p-5 flex items-center border-b h-24">
         {
             isLoggedIn && currentUser 
             ? (
             <>
-              <span className="font-semibold text-xl"> {currentUser.name}</span>
-               <span>님 환영합니다</span>           
+              <span className="p-2 font-semibold text-xl"> {currentUser.name}</span>
+               <span>님 환영합니다.</span>           
             </>
             )
             :
-            (<h2 className="text-xl font-bold mt-12">로그인</h2>)
+            (<h2 className="text-xl font-extrabold"><Link to={"/login"}>로그인</Link></h2>)
         }
           {/* 닫기 버튼 */}
-          <button className="text-gray-500 hover:text-black" onClick={() => setIsSidebarOpen(false)}>
+          <button className="ml-auto mr-3 text-gray-500 hover:text-black" onClick={() => setIsSidebarOpen(false)}>
             <span className="material-icons">close</span>
           </button>
         </div>
-        <ul className="p-5 space-y-3">
-          <li className="cursor-pointer hover:text-gray-600">
-            <Link to="/" onClick={() => setIsSidebarOpen(false)}>Home</Link>
+        <ul className="p-10 space-y-8">
+          <div className="p-20 border-solid border-4 border-gray-200 m-auto w-60 h-60 bg-gray-200 rounded-3xl">
+
+          </div>
+          <li className="cursor-pointer hover:text-gray-600  text-center">
+            <div className="font-bold text-2xl">닉네임</div>
+            <div className="font-semibold text-lg mt-2">Gold</div>
           </li>
           <li className="cursor-pointer hover:text-gray-600">
-            <Link to="/NotFound" onClick={() => setIsSidebarOpen(false)}>Magazine</Link>
+            <div>주문배송조회</div>
           </li>
           <li className="cursor-pointer hover:text-gray-600">
-            <Link to="/asks" onClick={() => setIsSidebarOpen(false)}>Services</Link>
+            <div>취소/교환/반품 내역</div>
           </li>
           <li className="cursor-pointer hover:text-gray-600">
-            <Link to="/events" onClick={() => setIsSidebarOpen(false)}>Events</Link>
+            <div>회원정보수정</div>
           </li>
           <li className="cursor-pointer hover:text-gray-600">
-            <Link to="/about" onClick={() => setIsSidebarOpen(false)}>About Us</Link>
+            <div>쿠폰</div>
           </li>
         </ul>
       </div>
